8b3b797c5baa82a608dc001173f1ff8784e05851
Matthew_Cash <matthew@matthew-cash.com>
2022-07-06 18:03:43 -0700

    Add program config

---
+++ Config.cs
@@ -0,0 +1,47 @@
+using System.Configuration;
+using System;
+using System.IO;
+using Tomlyn;
+using Tomlyn.Model;
+
+static class Config {
+    static TomlTable config;
+
+    public static TomlTable GetConfig() {
+        return config;
+    }
+
+    static String GetConfigPath() {
+        String appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string configFilePath = Path.Combine(appDataPath, "VpnManager", "config.toml");
+
+        return configFilePath;
+    }
+
+    static void CreateConfig(String configFilePath) {
+        Directory.CreateDirectory(Directory.GetParent(configFilePath).FullName);
+        File.Copy("resources/config.toml", configFilePath);
+    }
+
+    static String ReadConfigFile(String configFilePath) {
+        String[] lines = File.ReadAllLines(configFilePath);
+        String data = String.Join('\n', lines);
+
+        return data;
+    }
+
+    static TomlTable ParseConfigData(String data) {
+        return Toml.ToModel(data);
+    }
+
+    public static void LoadConfig() {
+        String configFilePath = GetConfigPath();
+
+        if (!File.Exists(configFilePath)) CreateConfig(configFilePath);
+
+        String configData = ReadConfigFile(configFilePath);
+        TomlTable config = ParseConfigData(configData);
+
+        Config.config = config;
+    }
+}
--- VpnManagerProgram.cs
+++ VpnManagerProgram.cs
@@ -8,14 +8,15 @@ namespace VpnManager
         [STAThread]
         static void Main()
         {
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            Console.WriteLine("Loading config...");
+            Config.LoadConfig();
+            Console.WriteLine("Config loaded!");
+
             Console.WriteLine("Starting Tray Service");
-
             Application.Run(new VpnManagerTrayIcon());
-
             Console.WriteLine("Tray Service Exited");
         }
     }
