90b980382db04039979292e038c8f1d0e9135022
Matthew_Cash <matthew@matthew-cash.com>
2022-10-28 17:15:13 -0700

    Repeat CheckIpAddress on error

--- src/CurrentIpAddress.cs
+++ src/CurrentIpAddress.cs
@@ -3,8 +3,14 @@ using System.Threading.Tasks;
 using System.Net.Http;
 
 static class CurrentIpAddress {
-    public static async Task<Boolean> CheckIpAddress(String tryAddress) {
-        String currentAddress = await GetIpAddress();
+    public static async Task<bool> CheckIpAddress(String tryAddress) {
+        String currentAddress = String.Empty;
+
+        // Getting the IP address may fail if the network is down, retry a few times with a delay
+        for (int i = 0; i < 20; i++) try {
+            currentAddress = await GetIpAddress();
+            break;
+        } catch (Exception) { await Task.Delay(50); };
 
         return tryAddress == currentAddress;
     }
