07d84de1028f081ea86f5a55db6ef3b1bc02e799
Matthew_Cash <matthew@matthew-cash.com>
2023-05-15 21:00:20 -0700

    Fix binary patch

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 dotnet publish -c Release
 
 # Patch the binary to make it WinExe instead of Console
-printf '\x67\x64\x02\x00\x02' | dd of=./bin/win-x64/publish/VpnManager.exe bs=1 seek=320 count=5 conv=notrunc
+printf '\x02\x00' | dd of=./bin/win-x64/publish/DesktopManager.exe bs=1 seek=316 count=2 conv=notrunc
