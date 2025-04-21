3d871df9150be8026f28b240bbc100f4b1acecc7
Matthew_Cash <matthew@matthew-cash.com>
2025-04-21 10:35:12 -0700

    Move main function to Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Forms;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace VpnManager {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Console.WriteLine("Loading consfig...");
+            Config.LoadConfig();
+            Console.WriteLine("Config loaded!");
+
+            CurrentIpAddress.StartIpAddressPolling();
+
+            Console.WriteLine("Starting Tray Service");
+            Application.Run(new VpnManagerTrayIcon());
+            Console.WriteLine("Tray Service Exited");
+
+            ToastNotificationManagerCompat.Uninstall();
+        }
+    }
+}
